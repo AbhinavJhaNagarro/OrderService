bdb4622151ff42ae188d025a61a0a84eb95d4bf9
Abhinav  Jha <abhinav.jha@nagarro.com>
2020-06-15 12:35:38 +0530

    Initial Commit

---
+++ OrderService/Controllers/OrdersController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OrderService.Controllers
+{
+  [ApiController]
+  [Route("[controller]")]
+  public class OrdersController : ControllerBase
+  {
+    [HttpGet("{id}")]
+    public IEnumerable<OrderModel> Get(int id)
+    {
+      return new List<OrderModel>() {
+        new OrderModel() { OrderId = 1, OrderAmount = 250, OrderData = DateTime.Now.AddDays(-2) },
+        new OrderModel() { OrderId = 2, OrderAmount = 450, OrderData = DateTime.Now.AddDays(-1) }
+      };
+
+    }
+  }
+}
---
+++ OrderService/Models/OrderModel.cs
@@ -0,0 +1,10 @@
+using System;
+
+public class OrderModel
+{
+  public int OrderId { get; set; }
+
+  public int OrderAmount { get; set; }
+
+  public DateTime OrderData { get; set; }
+}
